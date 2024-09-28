8e2ac87ac882d28987b203efbc4195e6f6fb31b2
Teshager <teshager8922@gmail.com>
2024-09-28 10:54:55 +0300

    display on trials and ask for the full set of samples to be generated

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -1,11 +1,13 @@
 import random 
+import statistics
+from statistics import mode
 from datetime import datetime,timedelta,date
 def generateRandomBirthday(num):
     birth_days=[]
     end_date= date.today()
     for i in range(num):
         start_date= date.today()
-        birth_date= (start_date-timedelta(days=random.randint(0,36500))).strftime('%b %d, %Y')
+        birth_date= (start_date-timedelta(days=random.randint(0,36500))).strftime('%b %d')
         birth_days.append(birth_date)
     return birth_days
 
@@ -15,12 +17,16 @@ def birthday_paradox():
     num= int(input("How many birthdays should I generate?\n"))
     counter=0
     repetition=0
-    while True:
-        sample=generateRandomBirthday(num)
-        if len(sample)!=len(set(sample)): repetition+=1
-        counter+=1
-        if(counter%10_000==0): print(f'{counter} simulations ran....')
-        if counter>TRIAL_REPETITION: 
-            print(repetition, TRIAL_REPETITION)
-            break
+    print(generateRandomBirthday(num))
+    print(f'The date {mode(generateRandomBirthday(num))} is repeated!')
+    print('Generating 23 random birthdays 100,000 times...')
+    if input('Press Enter to begin...''')=='':
+        while True:
+            sample=generateRandomBirthday(num)
+            if len(sample)!=len(set(sample)): repetition+=1
+            counter+=1
+            if(counter%10_000==0): print(f'{counter} simulations ran....')
+            if counter>TRIAL_REPETITION: 
+                print(repetition, TRIAL_REPETITION)
+                break
 birthday_paradox()
