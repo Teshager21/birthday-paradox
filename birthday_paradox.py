52ce3c7cc73607768c9a8a1fe7b68dbb1c4be92b
Teshager <teshager8922@gmail.com>
2024-09-28 10:21:46 +0300

    check for exact duplications of birthdays by running 100_000 trials

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -7,14 +7,20 @@ def generateRandomBirthday(num):
         start_date= date.today()
         birth_date= (start_date-timedelta(days=random.randint(0,36500))).strftime('%b %d, %Y')
         birth_days.append(birth_date)
-    print(birth_days)
+    return birth_days
+
 
 def birthday_paradox():
-    SAMPLE_SIZE=1
+    TRIAL_REPETITION=100_000
     num= int(input("How many birthdays should I generate?\n"))
     counter=0
+    repetition=0
     while True:
-        generateRandomBirthday(num)
+        sample=generateRandomBirthday(num)
+        if len(sample)!=len(set(sample)): repetition+=1
         counter+=1
-        if counter>SAMPLE_SIZE: break
+        if(counter%10_000==0): print(f'{counter} simulations ran....')
+        if counter>TRIAL_REPETITION: 
+            print(repetition, TRIAL_REPETITION)
+            break
 birthday_paradox()
